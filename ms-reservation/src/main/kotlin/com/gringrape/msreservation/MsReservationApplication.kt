5e6ea214e6fa58cac70047b260cbfb69c3a31463
gringrape <gringrape200@gmail.com>
2021-10-17 01:04:11 +0900

    [JIN-71] Set up reservation microservice
    Dependencies:
    
    Spring Data Reactive Redis
    Spring Reactive Web
    
    Issue: https://gringrape.atlassian.net/browse/JIN-71

---
+++ ms-reservation/src/main/kotlin/com/gringrape/msreservation/MsReservationApplication.kt
@@ -0,0 +1,11 @@
+package com.gringrape.msreservation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsReservationApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsReservationApplication>(*args)
+}
